6a5dfeecbdb49a74ddeee2dd0b9d89c850fb6b2a
Masdz <masdz1234567@gmail.com>
2018-04-24 11:08:53 -0500

    arrelar login

--- login/index.js
+++ login/index.js
@@ -3,7 +3,7 @@ firebase.auth().onAuthStateChanged(function(user) {
         //Usuario logeado
 //        document.getElementById("login_user").style.display="block";
   //      document.getElementById("home").style.display="none";
-        //window.open("https://caracteres.herokuapp.com/cargaA");
+        window.open("https://caracteres.herokuapp.com/cargaA");
         var user = firebase.auth().currentUser;
         if (user!=null){
             var email_id=user.email;
--- outlog.js
+++ outlog.js
@@ -1,27 +1,6 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        //Usuario logeado
-      //  document.getElementById("login_user").style.display="block";
-     //   document.getElementById("home").style.display="none";
-        window.open("http://localhost:5000/");
-
-        var user = firebase.auth().currentUser;
-        if (user!=null){
-            var email_id=user.email;
-            document.getElementById("nombre_user").innerHTML=" Hola: "+email_id;
-
-        }
-    } else {
-        // NUsuario no logueado
- //       document.getElementById("login_user").style.display="none";
- //       document.getElementById("home").style.display="block";
-         window.open("http://localhost/login/");
-
-    }
-});
-
 function logout(){
     firebase.auth().signOut();
+	window.open("https://caracteres.herokuapp.com/");
     /*().then(function() {
         // Sign-out successful.
     }).catch(function(error) {
